7db77c3e13335e235d7575c48fe2952268fddac7
caglarcercinli <chaglar.ronin@gmail.com>
2021-09-01 21:44:15 +0200

    created

--- src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/domain/GoedeDoel.java
@@ -2,23 +2,38 @@ package be.vdab.goededoel.domain;
 
 import java.math.BigDecimal;
 
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt * @author Joe Dalton
+ */
 public class GoedeDoel {
     private final String naam;
     private BigDecimal opgebracht;
     private BigDecimal opbrengst = BigDecimal.ZERO;
 
+    /**
+     * Maakt een GoedeDoel object * @param naam De naam van het goede doel
+     */
     public GoedeDoel(String naam) {
         this.naam = naam;
     }
 
+    /**
+     * Geeft de naam terug
+     *
+     * @return de naam
+     */
     public String getNaam() {
         return naam;
     }
 
-    public BigDecimal getOpgebracht() {
-        return opgebracht;
-    }
+//    public BigDecimal getOpgebracht() {
+//        return opgebracht;
+//    }
 
+    /**
+     * Geeft de opbrengst terug
+     * @return de opbrengst
+     */
     public BigDecimal getOpbrengst() {
         return opbrengst;
     }
